c5e1bdbfcd0d0bce7b22f6fe0de66fb92d989730
sfxfs <2501683578@qq.com>
2022-09-09 21:34:31 +0800

    循环执行

--- src/Serial.cpp
+++ src/Serial.cpp
@@ -11,49 +11,52 @@ void Serial_Setup(Serial_t *Stemp)
 
 void Serial_Ctrl(void *pvParameters)
 {
-    Serial_t *Stemp;
-    Stemp = (Serial_t *)pvParameters;
-    String data = "";
-    data += char(Serial.read());
-    delay(2);
-    if (data.length() == 1)
+    while (1)
     {
-        data.trim();
-        if (data.endsWith("n")) //开启自动驾驶
+        Serial_t *Stemp;
+        Stemp = (Serial_t *)pvParameters;
+        String data = "";
+        data += char(Serial.read());
+        delay(2);
+        if (data.length() == 1)
         {
-            Stemp->autocl = 1;
-        }
+            data.trim();
+            if (data.endsWith("n")) //开启自动驾驶
+            {
+                Stemp->autocl = 1;
+            }
 
-        if (data.endsWith("f")) //关闭自动驾驶
-        {
-            Stemp->autocl = 0;
-            Stemp->cmd = 's';
-        }
+            if (data.endsWith("f")) //关闭自动驾驶
+            {
+                Stemp->autocl = 0;
+                Stemp->cmd = 's';
+            }
 
-        if (data.endsWith("u")) //前进
-        {
-            Stemp->cmd = 'u';
-        }
+            if (data.endsWith("u")) //前进
+            {
+                Stemp->cmd = 'u';
+            }
 
-        if (data.endsWith("d")) //后退
-        {
-            Stemp->cmd = 'd';
-        }
+            if (data.endsWith("d")) //后退
+            {
+                Stemp->cmd = 'd';
+            }
 
-        if (data.endsWith("r")) //左转
-        {
-            Stemp->cmd = 'r';
-        }
+            if (data.endsWith("r")) //左转
+            {
+                Stemp->cmd = 'r';
+            }
 
-        if (data.endsWith("l")) //右转
-        {
-            Stemp->cmd = 'l';
-        }
+            if (data.endsWith("l")) //右转
+            {
+                Stemp->cmd = 'l';
+            }
 
-        if (data.endsWith("s")) //停止
-        {
-            Stemp->cmd = 's';
+            if (data.endsWith("s")) //停止
+            {
+                Stemp->cmd = 's';
+            }
+            data = "";
         }
-        data = "";
     }
 }
--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -12,51 +12,54 @@ void Tracker_Setup(void)
 
 void Tracker_Get(void *pvParameters)
 {
-    PID_t *PID;
-    PID = (PID_t *)pvParameters;
-    unsigned char temp = 0b00000; //临时变量用于新一轮采集
-    for (int i = 0; i < 5; i++)
-        temp |= digitalRead(A[i]) << i; //轮询5个传感器输出，并将查询结果转换为编码形式
-    switch (temp)                       // 0为黑
+    while (1)
     {
-    case 0b11011:
-        PID->error = 0;
-        break;
-    case 0b10001:
-        PID->error = 0;
-        break;
-    case 0b01111:
-        PID->error = -ER_D;
-        break;
-    case 0b00111:
-        PID->error = -ER_C;
-        break;
-    case 0b00011:
-        PID->error = -ER_C;
-        break;
-    case 0b10111:
-        PID->error = -ER_B;
-        break;
-    case 0b10011:
-        PID->error = -ER_A, PID->m = 10;
-        break;
-    case 0b11001:
-        PID->error = ER_A, PID->m = 10;
-        break;
-    case 0b11101:
-        PID->error = ER_B;
-        break;
-    case 0b11000:
-        PID->error = ER_C;
-        break;
-    case 0b11100:
-        PID->error = ER_C;
-        break;
-    case 0b11110:
-        PID->error = ER_D;
-        break;
-    case 0b00000:
-        PID->p = 0;
-        break;
+        PID_t *PID;
+        PID = (PID_t *)pvParameters;
+        unsigned char temp = 0b00000; //临时变量用于新一轮采集
+        for (int i = 0; i < 5; i++)
+            temp |= digitalRead(A[i]) << i; //轮询5个传感器输出，并将查询结果转换为编码形式
+        switch (temp)                       // 0为黑
+        {
+        case 0b11011:
+            PID->error = 0;
+            break;
+        case 0b10001:
+            PID->error = 0;
+            break;
+        case 0b01111:
+            PID->error = -ER_D;
+            break;
+        case 0b00111:
+            PID->error = -ER_C;
+            break;
+        case 0b00011:
+            PID->error = -ER_C;
+            break;
+        case 0b10111:
+            PID->error = -ER_B;
+            break;
+        case 0b10011:
+            PID->error = -ER_A, PID->m = 10;
+            break;
+        case 0b11001:
+            PID->error = ER_A, PID->m = 10;
+            break;
+        case 0b11101:
+            PID->error = ER_B;
+            break;
+        case 0b11000:
+            PID->error = ER_C;
+            break;
+        case 0b11100:
+            PID->error = ER_C;
+            break;
+        case 0b11110:
+            PID->error = ER_D;
+            break;
+        case 0b00000:
+            PID->p = 0;
+            break;
+        }
     }
 }
