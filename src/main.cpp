8d92d9ce95f98a8c72fdb8c95da27f170d8cc572
sfxfs <2501683578@qq.com>
2022-09-22 20:48:37 +0800

    更改栈大小

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@ void setup()
   PID_Init(PID_car);                                                                  //初始化结构体
   Motor_Setup();                                                                      //初始化电机对应引脚
   Tracker_Setup();                                                                    //初始化传感器相关引脚
-  xTaskCreate(Serial_Ctrl, "Serial", 512, (void *)Serial_car, 1, TaskSerial_Handler); //创建读取串口的线程
-  xTaskCreate(Tracker_Get, "Tracker", 512, (void *)PID_car, 2, TaskTracker_Handler);  //创建读取传感器的线程
+  xTaskCreate(Serial_Ctrl, "Serial", 1024, (void *)Serial_car, 1, TaskSerial_Handler); //创建读取串口的线程
+  xTaskCreate(Tracker_Get, "Tracker", 1024, (void *)PID_car, 2, TaskTracker_Handler);  //创建读取传感器的线程
 }
 
 void loop()
